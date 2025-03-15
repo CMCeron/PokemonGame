c9b7ed0fdb829a1732339e1ca8d959736eca6237
Clementina Mª <cmceron244@gmail.com>
2025-03-15 14:25:25 +0100

    FuncionaPlayerGender

--- Modules/Objects/Player/Player.js
+++ Modules/Objects/Player/Player.js
@@ -4,30 +4,40 @@ class Player {
             this.width = 14,
             this.height = 19,
 
-            this.gender = () => {
-                $.ajax({
-                    url: 'http://localhost/PokemonGame/Modules/Objects/Player/GetPlayer.php',
-                    method: 'get',
-                    success: (data, status) => {
-                        return data
-                    }
-                })
-            },
+            this.gender = null;
 
             this.stand = new Image();
-        this.stand.src = `${this.src + this.gender}_Standing.png`,
-
+            
             this.walk = new Image();
-        this.walk.src = `${this.src + this.gender}_Walk.png`,
-
+            
             this.battle = new Image();
-        this.battle.src = `${this.src + this.gender}_Battle.png`,
-
+            
             this.captured = new Image();
-        this.captured.src = `${this.src + this.gender}_Captured.png`,
-
+            
             this.BattleStart = new Image();
-        this.BattleStart.src = `${this.src + this.gender}_BattleStart.png`
-    }
+            
+            this.getGender();
+        }
+        
+        getGender(){
+            $.ajax({
+                url: 'http://localhost/PokemonGame/Modules/Objects/Player/GetPlayer.php',
+                method: 'get',
+                success: (data, status) => {
+                    this.gender = data;
+                    this.loadImages();
+                }
+            })
+        }
 
-}
+        loadImages(){
+            if(this.gender){
+
+                this.stand.src = `${this.src + this.gender}_Standing.png`;
+                this.walk.src = `${this.src + this.gender}_Walk.png`;
+                this.battle.src = `${this.src + this.gender}_Battle.png`;
+                this.BattleStart.src = `${this.src + this.gender}_BattleStart.png`;
+                this.captured.src = `${this.src + this.gender}_Captured.png`;
+            }
+        }
+    }
